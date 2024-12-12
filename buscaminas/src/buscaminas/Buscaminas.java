b72e44f9ff7c7b6b420adbdb95aa378131c89e7a
nicolas <nicoestebannovoa@gmail.com>
2024-12-12 10:28:07 +0100

    la insercion de las bombas terminada y el algoritmo para rellenar filas a medias

--- buscaminas/src/buscaminas/Buscaminas.java
+++ buscaminas/src/buscaminas/Buscaminas.java
@@ -8,19 +8,19 @@ public class Buscaminas {
         int filas = 0;
         int columnas = 0;
         int bombas = 0;
+        //inicio del programa
         do{
             System.out.println("bienvenido a el buscaminas de Nicolas Lopez 1DAPLIWEB");
             System.out.println("por favor ingrese la dificultad del juego: [1 facil][2 dificil][3 experto][4 personalizada]");
             dif = sc.nextInt();
+            //escoger dificultad
             switch(dif){
                 case 1 -> {
-                    System.out.println("hola");
                     filas = 8;
                     columnas = 8;
                     bombas = 10;
                 }
                 case 2 -> {
-                    System.out.println("hola 2");
                     filas = 16;
                     columnas = 16;
                     bombas = 40;
@@ -39,16 +39,45 @@ public class Buscaminas {
                     bombas = sc.nextInt();
                 }
             }
+            //rellenar la matriz de ceros
             int[][] matriz = new int[filas][columnas];
             for(int i = 0; i <filas ; i++){
                 for(int j = 0; j <columnas ; j++){
                     matriz[i][j] = 0;
                 }
             }
+            //rellenar la matriz con las bombas
+            for(int i = 0; i <bombas ; i++){
+                int columnarandom = (int)(Math.random() * columnas);
+                int filarandom = (int)(Math.random() * filas);
+                if(!(matriz[filarandom][columnarandom]==-1)){     
+                    matriz[filarandom][columnarandom]=-1;
+                }
+                else{
+                    i--;
+                }
+            }
+            //rellenar las casillas de alrededor
             for(int i = 0; i <filas ; i++){
                 for(int j = 0; j <columnas ; j++){
-                    System.out.println(matriz[i][j]);
+                    if(matriz[i][j]==-1 && i> 0 && j> 0 && i<filas-1 && j<columnas-1){
+                        matriz[i-1][j-1]++;
+                        matriz[i-1][j]++;
+                        matriz[i-1][j+1]++;
+                        matriz[i][j-1]++;
+                        matriz[i][j+1]++;
+                        matriz[i+1][j-1]++;
+                        matriz[i+1][j]++;
+                        matriz[i+1][j+1]++;
+                    }
                 }
+            }
+            //imprimir la matriz principal
+            for(int i = 0; i <filas ; i++){
+                for(int j = 0; j <columnas ; j++){
+                    System.out.print(matriz[i][j]+" ");
+                }
+                System.out.println();
             }
         }while(!juego);
     }
